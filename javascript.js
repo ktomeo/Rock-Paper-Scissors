b8d1cf5f025ee9633c2668b0d84f34d7321c4dde
Kai Tomeo <kaitomeo14@gmail.com>
2023-02-26 12:09:00 -0800

    Added computer selection
    Able to randomly generate 1 of 3 plays each round

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-function getComputerChoice (x)
+function computerSelection() {
+    let x = Math.floor(Math.random() * 3);
+    if (x === 0) {
+        return 'ROCK';
+    }
+    else if (x === 1) {
+        return 'PAPER';
+    }
+    else {
+        return 'SCISSORS';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
