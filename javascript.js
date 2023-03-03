3d789a99b05d09b7b451111c00a6c41593d7dbe1
Kai Tomeo <kaitomeo14@gmail.com>
2023-03-02 21:26:50 -0800

    Added break in loop and win condition message
    Modified the counter and stops the game at first to 5 and displays a message

--- javascript.js
+++ javascript.js
@@ -11,7 +11,8 @@ function getComputerChoice() {
     }
 }
 
-let counter = {computer: 0, player: 0};
+let counterComputer = 0;
+let counterPlayer = 0;
 
 function playRound() {
     let playerSelection = prompt('Rock, Paper, or Scissors?').toUpperCase();
@@ -19,27 +20,42 @@ function playRound() {
     console.log(playerSelection)
     console.log(computerSelection)
     if (playerSelection === computerSelection) {
-        return 'Tie'
+        return alert('Tie')
     } else if (
         (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
         (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
         (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
-        counter.player++;
-        return 'Win'
-        }
-    else if (
+        counterPlayer++;
+        return alert('Win')
+        } else if (
         (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') ||
         (playerSelection === 'ROCK' && computerSelection === 'PAPER') ||
         (playerSelection === 'PAPER' && computerSelection === 'SCISSORS')) {
-        counter.computer++;
-        return 'Lose'
+        counterComputer++;
+        return alert('Lose')
         }
     else {
-        return 'Try again'
+        return alert('Try again')
     }
 }
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; (i < 1000); i++) {
     console.log(playRound());
-    console.log(counter);
+    console.log(counterPlayer);
+    console.log(counterComputer);
+    if (counterComputer === 5) {
+        break;
+    } else if (counterPlayer === 5) {
+        break;
+    }
 }
+
+function winCondition() {
+    if (counterComputer === 5) {
+        return alert('You lost to the computer!');
+    } else if (counterPlayer === 5) {
+        return alert('You beat the computer!');
+    }
+}
+
+console.log (winCondition());
